66b3ee43f8ef04d367b0c6fe94dc91117e157ee9
c-goss <g00169967@W213-31.academic.raritanval.edu>
2015-09-21 17:42:34 -0400

    My new picture
    It's a swimming person!

--- Picture.java
+++ Picture.java
@@ -10,12 +10,12 @@
  * @version 2011.07.31
  */
 public class Picture
-{
-    private Square wall;
-    private Square window;
-    private Triangle roof;
-    private Circle sun;
-
+{       
+        private Square water;
+        private Triangle ground;
+        private Person swimmer;
+        private Circle bubble;
+        
     /**
      * Constructor for objects of class Picture
      */
@@ -29,31 +29,29 @@ public class Picture
      */
     public void draw()
     {
-        wall = new Square();
-        wall.moveHorizontal(-140);
-        wall.moveVertical(20);
-        wall.changeSize(120);
-        wall.makeVisible();
+        water = new Square();
+        water.changeColor("blue");
+        water.moveHorizontal(-400);
+        water.moveVertical(-50);
+        water.changeSize(1000);
+        water.makeVisible();
         
-        window = new Square();
-        window.changeColor("black");
-        window.moveHorizontal(-120);
-        window.moveVertical(40);
-        window.changeSize(40);
-        window.makeVisible();
-
-        roof = new Triangle();  
-        roof.changeSize(60, 180);
-        roof.moveHorizontal(20);
-        roof.moveVertical(-60);
-        roof.makeVisible();
-
-        sun = new Circle();
-        sun.changeColor("yellow");
-        sun.moveHorizontal(100);
-        sun.moveVertical(-40);
-        sun.changeSize(80);
-        sun.makeVisible();
+        ground = new Triangle();
+        ground.changeColor("green");
+        ground.changeSize(50,1000);
+        ground.moveVertical(110);
+        ground.moveHorizontal(250);
+        ground.makeVisible();
+        
+        swimmer = new Person();
+        swimmer.makeVisible();
+        
+        bubble = new Circle();
+        bubble.changeColor("white");
+        bubble.makeVisible();
+        bubble.changeSize(10);
+        bubble.moveHorizontal(60);
+        bubble.moveVertical(55);
     }
 
     /**
@@ -61,12 +59,12 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        if (wall != null)   // only if it's painted already...
+        if (water != null)   // only if it's painted already...
         {
-            wall.changeColor("black");
-            window.changeColor("white");
-            roof.changeColor("black");
-            sun.changeColor("black");
+            water.changeColor("black");
+            ground.changeColor("white");
+            swimmer.changeColor("white");
+            bubble.changeColor("white");
         }
     }
 
@@ -75,12 +73,12 @@ public class Picture
      */
     public void setColor()
     {
-        if (wall != null)   // only if it's painted already...
+        if (water != null)   // only if it's painted already...
         {
-            wall.changeColor("red");
-            window.changeColor("black");
-            roof.changeColor("green");
-            sun.changeColor("yellow");
+            water.changeColor("blue");
+            ground.changeColor("green");
+            swimmer.changeColor("black");
+            bubble.changeColor("white");
         }
     }
 }
